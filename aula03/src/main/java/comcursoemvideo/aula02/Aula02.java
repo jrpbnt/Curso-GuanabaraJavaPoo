3d3ba0a132f6ce41241bc493d5370c4318404a5e
jrpbnt <78651955+jrpbnt@users.noreply.github.com>
2023-03-07 16:22:59 -0300

    Aprendendo sobre visibilidade de atributos e metodos

---
+++ aula03/src/main/java/comcursoemvideo/aula02/Aula02.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package comcursoemvideo.aula02;
+
+/**
+ *
+ * @author jrpbnt
+ */
+public class Aula02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Caneta c1 = new Caneta();
+        c1.cor =  "Azul";
+        c1.ponta = 0.5f;
+        c1.tampar();
+        
+        c1.rabiscar();
+        
+        Caneta c2 = new Caneta();
+        c2.Modelo  = "hostnet";
+         c2.cor = "Preta";
+         c2.destampar();
+         c2.rabiscar();
+         
+        
+    }
+    
+        
+        
+    }
+
